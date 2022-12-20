0b8a76738f24bdb154a668530de56b6bc8e37ae6
madefaxayuchann <ayumichan837@gmail.com>
2022-12-21 02:09:56 +0700

    feat:fix key props

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -17,26 +17,32 @@ const Card = () => {
     }
   ]
   return (
-    <div class='container my-12 mx-auto px-4 md:px-12'>
-      <div class='flex flex-wrap -mx-1 lg:-mx-4'>
+    <div className='container my-12 mx-auto px-4 md:px-12'>
+      <div className='flex flex-wrap -mx-1 lg:-mx-4'>
         {arr.map((item, index) => (
-          <div class='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3'>
-            <article class='overflow-hidden rounded-lg shadow-lg'>
+          <div
+            key={index}
+            className='my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3'
+          >
+            <article className='overflow-hidden rounded-lg shadow-lg'>
               <a href='#'>
                 <img
                   alt='Placeholder'
-                  class='block h-auto w-full'
+                  className='block h-auto w-full'
                   src='https://picsum.photos/600/400/?random'
                 />
               </a>
 
-              <header class='flex items-center justify-between leading-tight p-2 md:p-4'>
-                <h1 class='text-lg'>
-                  <a class='no-underline hover:underline text-black' href='#'>
-                    Article Title
+              <header className='flex items-center justify-between leading-tight p-2 md:p-4'>
+                <h1 className='text-lg'>
+                  <a
+                    className='no-underline hover:underline text-black'
+                    href='#'
+                  >
+                    {item.name}
                   </a>
                 </h1>
-                <p class='text-grey-darker text-sm'>11/1/19</p>
+                <p className='text-grey-darker text-sm'>{item.description}</p>
               </header>
             </article>
           </div>
