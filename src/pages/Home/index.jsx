5a560cd52ceacb21f0f6206de3fc895ca6e60363
Juki Beng Hantachi <jukibenghantachi@gmail.com>
2022-12-21 10:48:03 +0700

    feat: pokemons list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
-import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Navbar from "../../components/Navbar";
 import Card from "../../components/Card";
 import useGetList from "../../hooks/useGetList";
 
 const Home = ({ type }) => {
+  const [pokemons, setPokemons] = useState([]);
   const { data } = useGetList(type);
+
+  const fetchPokemon = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+  };
+
+  useEffect(() => {
+    if (data) {
+      data.pokemon_species.forEach(async (item) => {
+        const pokemon = await fetchPokemon(item.url);
+        setPokemons((prevValue) => [...prevValue, pokemon]);
+      });
+    }
+  }, [data]);
 
   return (
     <>
